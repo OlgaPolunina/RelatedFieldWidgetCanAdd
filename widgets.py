05e855ea2d4aaf9ab88ed016fec359bb52eba40c
OlgaPolunina <poa.webaspect@gmail.com>
2015-08-24 17:46:21 +0600

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -2,14 +2,10 @@ from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
 from django.forms import widgets
 from django.conf import settings
+
 class RelatedFieldWidgetCanAdd(widgets.Select):
-    def __init__(self, related_model, related_url=None, *args, **kw):
+    def __init__(self, related_model, related_url, *args, **kw):
         super(RelatedFieldWidgetCanAdd, self).__init__(*args, **kw)
-        if not related_url:
-            rel_to = related_model
-            info = (rel_to._meta.app_label, rel_to._meta.object_name.lower())
-            related_url = 'admin:%s_%s_add' % info
-        # Be careful that here "reverse" is not allowed
         self.related_url = related_url
     def render(self, name, value, *args, **kwargs):
         self.related_url = reverse(self.related_url)
